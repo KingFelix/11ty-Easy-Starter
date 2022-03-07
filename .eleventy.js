0cc374f9a15f23ca9b436f294b2fde93ec09eadd
KingFelix <jacopo.rabolini@gmail.com>
2022-03-07 23:31:41 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,14 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/js/");
   eleventyConfig.addWatchTarget("./src/img/");
 
+  eleventyConfig.addPassthroughCopy("./src/img/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
+
   return {
     dir: {
       input: "src",
       output: "dist"
     }
   }
+
 };
